da7fc94f2f9762b44b1f4768112b14e4ae53219b
Ayush <AJ@xaezor.com>
2025-05-09 18:49:11 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String,
+    default: '',
+  },
+  rank: {
+    type: String,
+    default: 'User',
+  },
+  bio: {
+    type: String,
+    default: '',
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
